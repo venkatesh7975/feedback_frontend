76649d74dbbd3ee08e9df737b55be688080381d5
venkatesh7975 <venkateshmoyya7975@gmail.com>
2025-07-05 11:58:52 +0530

    first commit

---
+++ src/components/Admin.jsx
@@ -0,0 +1,27 @@
+import axios from "axios";
+import {useState,useEffect} from "react";
+export default function Admin() {
+    const [feedbacks,setFeedback]=useState([]);
+
+    async function getFeedback(){
+        const response =await  axios("https://feedback-backend-4oa2.onrender.com/feedbacks");
+        setFeedback(response.data)
+        console.log(response.data)
+    }
+
+    useEffect(()=>{
+   getFeedback()
+    },[])
+ 
+  return (
+    <div>
+
+<ul>
+    {feedbacks.map((feedback,index)=>(
+        <li key={index}>{feedback.name}<br/>
+        {feedback.message}</li>
+    ))}
+</ul>
+        </div>
+  )
+}
